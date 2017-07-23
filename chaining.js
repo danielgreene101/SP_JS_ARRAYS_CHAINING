3658c211cfbb0d0df9f0ba7d4da2388a5d27f9c8
Daniel Greene <d.b.greene101@gmail.com>
2017-07-23 16:56:21 -0500

    need to condence into one line

---
+++ chaining.js
@@ -0,0 +1,34 @@
+var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
+
+
+function sortInt() {
+  integers.sort(function (a, b){
+      return b - a
+  }); 
+console.log(integers);
+}
+
+
+sortInt();
+
+
+
+function remove(nums) {
+    return nums < 19;
+}
+
+function res() {
+    console.log(integers.filter(remove));
+}
+
+res();
+
+function math(){
+    let newValue = 0;
+    for (let i = 0; i < integers.length; i++) {
+    newValue = integers[i] * 1.5 - 1;
+        console.log(newValue);
+    }
+}
+
+math();
