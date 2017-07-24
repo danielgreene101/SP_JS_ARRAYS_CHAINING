caea55238c496c18a713ac948fb667871fd1a6b5
Daniel Greene <d.b.greene101@gmail.com>
2017-07-24 13:02:28 -0500

    finished

--- chaining.js
+++ chaining.js
@@ -1,6 +1,7 @@
 var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
-
-
+let result = integers.sort((a,b) => {return b - a}).filter((num) => {return num < 19}).map((num) => {return (num * 1.5) - 1}).reduce((previous, current) => {return previous + current});
+console.log("result", result)
+/*
 function sortInt() {
   integers.sort(function (a, b){
       return b - a
@@ -32,3 +33,4 @@ function math(){
 }
 
 math();
+*/
